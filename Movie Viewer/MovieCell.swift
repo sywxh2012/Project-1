36495aaded5cd09e24e049002f0f31f23ac16206
sywxh2012 <xinghan.wang@wustl.edu>
2016-01-25 21:31:49 -0600

    first commit

---
+++ Movie Viewer/MovieCell.swift
@@ -0,0 +1,29 @@
+//
+//  MovieCell.swift
+//  Movie Viewer
+//
+//  Created by Xinghan Wang on 1/25/16.
+//  Copyright © 2016 Xinghan Wang. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var overviewLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
