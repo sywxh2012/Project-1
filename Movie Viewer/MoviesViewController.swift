5f25c9fe1d6e0911dd43572a528a48bda00c451e
sywxh2012 <xinghan.wang@wustl.edu>
2016-02-01 22:48:42 -0600

    Flicks week 2 pre-work

--- Movie Viewer/MoviesViewController.swift
+++ Movie Viewer/MoviesViewController.swift
@@ -14,7 +14,6 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
 
     @IBOutlet weak var TableView: UITableView!
     
-    
     var movies: [NSDictionary]?
     
     override func viewDidLoad() {
@@ -131,17 +130,18 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         let movie = movies![indexPath.row]
         let title = movie["title"] as! String
         let overview = movie["overview"] as! String
-        let posterPath = movie["poster_path"] as! String
-        
         let baseUrl = "http://image.tmdb.org/t/p/w500"
-        let imageUrl = NSURL(string: baseUrl + posterPath)
         
+        if let posterPath = movie["poster_path"] as? String {
+
+        let imageUrl = NSURL(string: baseUrl + posterPath)
+        cell.posterView.setImageWithURL(imageUrl!)
+        }
         
         cell.titleLabel.text = title
         cell.overviewLabel.text = overview
-        cell.posterView.setImageWithURL(imageUrl!)
-        
-        print("row \(indexPath.row)")
+
+        //print("row \(indexPath.row)")
         
         return cell
     }
@@ -150,14 +150,22 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
 
     
     
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        
+        let cell = sender as! UITableViewCell
+        let indexPath = TableView.indexPathForCell(cell)
+        let movie = movies![indexPath!.row]
+        
+        let detailViewController = segue.destinationViewController as! DetailViewController
+        
+        detailViewController.movie = movie
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
     }
-    */
+    
 
 }
